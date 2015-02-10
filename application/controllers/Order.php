dc267d8ea57afd910142887a333e68b63a940053
sstan4411 <stefanstan92@gmail.com>
2015-02-10 13:12:38 -0800

    got the make order page showing something instead of errors :) Signed-off-by:sstan4411 <stefanstan92@gmail.com>

--- application/controllers/Order.php
+++ application/controllers/Order.php
@@ -18,6 +18,14 @@ class Order extends Application {
     // start a new order
     function neworder() {
         //FIXME
+        $order_num = $this->orders->highest() + 1;
+        
+        $neworder = $this->orders->create();
+        $neworder->num = $order_num;
+        $neworder->date = date();
+        $neworder->status = 'a';
+        $neworder->total = 0;
+        $this->orders->add($neworder);
 
         redirect('/order/display_menu/' . $order_num);
     }
@@ -30,7 +38,7 @@ class Order extends Application {
         $this->data['pagebody'] = 'show_menu';
         $this->data['order_num'] = $order_num;
         //FIXME
-
+        $this->data['title'] = "order # ".$order_num;
         // Make the columns
         $this->data['meals'] = $this->make_column('m');
         $this->data['drinks'] = $this->make_column('d');
@@ -64,7 +72,7 @@ class Order extends Application {
     // make a menu ordering column
     function make_column($category) {
         //FIXME
-        return $items;
+        return $this->menu->some('category', $category);
     }
 
     // add an item to an order
